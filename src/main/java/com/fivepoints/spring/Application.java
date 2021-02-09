73a1d6ad16e58a239e6abdb9a8691589cf56214e
dagboujhatem <dagboujhatem@gmail.com>
2021-02-09 21:24:10 +0100

    delete swagger configuration from master branch

--- src/main/java/com/fivepoints/spring/Application.java
+++ src/main/java/com/fivepoints/spring/Application.java
@@ -11,6 +11,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class Application implements ApplicationRunner {
