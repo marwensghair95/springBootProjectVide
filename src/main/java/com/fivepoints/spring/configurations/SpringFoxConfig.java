a01277ec98d1bb2b7e65d102949caa00277ed04a
dagboujhatem <dagboujhatem@gmail.com>
2021-02-10 02:40:58 +0100

    Describe your apis swagger

--- src/main/java/com/fivepoints/spring/configurations/SpringFoxConfig.java
+++ src/main/java/com/fivepoints/spring/configurations/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.fivepoints.spring.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -12,9 +15,23 @@ public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    // Describe your apis
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger Sample APIs")
+                .description("This page lists all the rest apis for Swagger Sample App.")
+                .version("1.0.0")
+//                .license("Apache License Version 2.0")
+//                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+//                .termsOfServiceUrl("https://gitee.com/log4j/pig")
+//                .contact(new Contact("DAGBOUJ Hatem","https://github.com/dagboujhatem","dagboujhatem@gmail.com"))
                 .build();
     }
 }
