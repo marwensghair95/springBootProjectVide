871ad48e769de7a7332af1ae63c8806b5dbb8f36
dagboujhatem <dagboujhatem@gmail.com>
2021-02-24 07:30:17 +0100

    add Authorisation button in swagger docs

--- src/main/java/com/fivepoints/spring/configurations/SpringFoxConfig.java
+++ src/main/java/com/fivepoints/spring/configurations/SpringFoxConfig.java
@@ -2,20 +2,17 @@ package com.fivepoints.spring.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityScheme;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
+import java.util.*;
 
 /**
  *
@@ -25,13 +22,14 @@ import java.util.LinkedHashSet;
 
 
 @Configuration
+@Import(springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .produces(new LinkedHashSet<>(Arrays.asList("application/json", "application/xml")))
                 .protocols(new HashSet<>(Arrays.asList("http","https")))
-//                .securitySchemes(securitySchemes())
+                .securitySchemes(Arrays.asList(apiKey()))
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
@@ -52,8 +50,22 @@ public class SpringFoxConfig {
                 .build();
     }
 
-//    private static ArrayList<? extends SecurityScheme> securitySchemes() {
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+//    private SecurityContext securityContext() {
+//        return SecurityContext.builder()
+//                .securityReferences(defaultAuth())
+//                .forPaths(PathSelectors.regex("/api/.*"))
+//                .build();
+//    }
 //
-//        return [new ApiKey("Bearer", "Authorization", "header")];
+//    List<SecurityReference> defaultAuth() {
+//        AuthorizationScope authorizationScope
+//                = new AuthorizationScope("global", "accessEverything");
+//        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+//        authorizationScopes[0] = authorizationScope;
+//        return Lists.newArrayList(new SecurityReference("JWT", authorizationScopes));
 //    }
 }
--- src/main/java/com/fivepoints/spring/controllers/UserController.java
+++ src/main/java/com/fivepoints/spring/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.fivepoints.spring.controllers;
 import com.fivepoints.spring.entities.User;
 import com.fivepoints.spring.payload.responses.MessageResponse;
 import com.fivepoints.spring.services.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("users")
+@ApiOperation(value = "", authorizations = { @Authorization(value="jwtToken") })
 public class UserController {
 
     @Autowired
